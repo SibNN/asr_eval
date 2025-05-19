58f84e0fb2a04445e5e71f58a5cda1b4a4f1e142
Oleg Sedukhin <sedol.mong@mail.ru>
2025-05-19 13:40:11 +0300

    update PartialTranscription

--- asr_eval/streaming/transcription.py
+++ asr_eval/streaming/transcription.py
@@ -8,21 +8,37 @@ import uuid
 class PartialTranscription:
     """
     A chunk returned by a streaming ASR model, may contain any text and any ID. If the model
-    wants to edit the previous chunk, it can yield the same ID with another text. Example:
+    wants to edit the previous chunk, it can yield the same ID with another text, or refer to
+    the last chunk with ID == 'latest'. Example:
     
     PartialTranscription.join([
         PartialTranscription(text='a'),
+        PartialTranscription(id='latest', text='a2'),
         PartialTranscription(id=1, text='b'),
         PartialTranscription(id=2, text='c'),
         PartialTranscription(id=1, text='b2 b3'),
-    ]) == 'a b2 b3 c'
+    ]) == 'a2 b2 b3 c'
     """
-    id: int = field(default_factory=lambda: str(uuid.uuid4()))
+    id: int | str = field(default_factory=lambda: str(uuid.uuid4()))
     text: str
     
     @classmethod
     def join(cls, transcriptions: list[PartialTranscription]) -> str:
-        parts: dict[int, str] = {}
+        parts: dict[int | str, str] = {}
+        
+        latest_id = None
         for t in transcriptions:
-            parts[t.id] = t.text  # add or edit a text, keeps the order
+            if t.id == 'latest':
+                # edit the lastest chunk
+                if latest_id is None:
+                    # the very first chunk has "latest" ID, assiging an ID
+                    latest_id = '<initial>'
+                parts[latest_id] = t.text
+            elif t.id in parts:
+                # edit one of the previous chunks
+                parts[t.id] = t.text
+            else:
+                # add a new chunk, set as latest
+                parts[t.id] = t.text
+                latest_id = t.id
         return ' '.join(parts.values())
--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -4,11 +4,24 @@ from asr_eval.streaming.transcription import PartialTranscription
 def test_join_transcriptions():
     assert PartialTranscription.join([
         PartialTranscription(text='a'),
+        PartialTranscription(id='latest', text='a2'),
+        PartialTranscription(id=1, text='b'),
+        PartialTranscription(id=2, text='c'),
+        PartialTranscription(id=1, text='b2 b3'),
+    ]) == 'a2 b2 b3 c'
+    
+    assert PartialTranscription.join([
+        PartialTranscription(text='a'),
         PartialTranscription(id=1, text='b'),
         PartialTranscription(id=2, text='c'),
         PartialTranscription(text='x'),
         PartialTranscription(id=1, text='b2'),
-        PartialTranscription(text='!@<>'),
+        PartialTranscription(id='latest', text='!@<>'),
         PartialTranscription(id=1, text='b2 b3'),
         PartialTranscription(id=2, text='c2'),
-    ]) == 'a b2 b3 c2 x !@<>'
+    ]) == 'a b2 b3 c2 !@<>'
+    
+    assert PartialTranscription.join([
+        PartialTranscription(id='latest', text='a'),
+        PartialTranscription(id='latest', text='b'),
+    ]) == 'b'
