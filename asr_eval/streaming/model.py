d2068229e975bab9940f37fe97ea9291e82c5364
Oleg Sedukhin <sedol.mong@mail.ru>
2025-06-08 23:34:29 +0300

    rm old commented out code

--- asr_eval/streaming/model.py
+++ asr_eval/streaming/model.py
@@ -64,19 +64,11 @@ class ASRStreamingQueue(StreamingQueue[CHUNK_TYPE]):
         super().__init__(name=name)
         self._counts: dict[ID_TYPE, int] = defaultdict(int)
         self._finished_ids: set[ID_TYPE] = set()
-        # self.history: list[tuple[CHUNK_TYPE, ID_TYPE]] | None = None
-        # self._positions_in_history: dict[int, int] = {}
-    
-    # def track_history(self):
-    #     self.history = []
     
     @override
     def get(self, id: ID_TYPE | None = None, timeout: float | None = None) -> tuple[CHUNK_TYPE, ID_TYPE]:
         data, id = super().get(id=id, timeout=timeout)
         data.get_timestamp = time.time()
-        # if self.history is not None and builtins.id(data) in self._positions_in_history:
-        #     history_idx = self._positions_in_history[builtins.id(data)]
-        #     self.history[history_idx][0].get_timestamp = data.get_timestamp
         return data, id
     
     @override
@@ -84,12 +76,6 @@ class ASRStreamingQueue(StreamingQueue[CHUNK_TYPE]):
         self._validate(data=data, id=id)
         data.put_timestamp = time.time()
         self._counts[id] += 1
-        # if self.history is not None:
-        #     self.history.append((copy.deepcopy(data), id))
-        #     # self.history.append((data_copy := copy.deepcopy(data), id))
-        #     # if isinstance(data_copy, AUDIO_CHUNK_TYPE):  # ??
-        #     #     del data_copy.data
-        #     # self._positions_in_history[builtins.id(data)] = len(self.history) - 1
         return super().put(data, id=id)
     
     def _validate(self, data: CHUNK_TYPE, id: ID_TYPE):
