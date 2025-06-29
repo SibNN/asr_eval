7037704ae347eda7a0a496b106af1dc0f110daae
Oleg Sedukhin <sedukhin@sibnn.ai>
2025-06-29 15:06:17 +0300

    speechbrain streaming

---
+++ asr_eval/streaming/models/speechbrain.py
@@ -0,0 +1,62 @@
+from collections import defaultdict
+from typing import Literal, override
+
+import torch
+import numpy as np
+import speechbrain.inference.ASR
+from speechbrain.utils.dynamic_chunk_training import DynChunkTrainConfig
+
+from asr_eval.streaming.buffer import ID_TYPE
+from asr_eval.streaming.model import OutputChunk, StreamingASR, Signal, TranscriptionChunk
+
+
+class SpeechbrainStreaming(StreamingASR):
+    """
+    Adopted from Gradio example from here:
+    https://huggingface.co/speechbrain/asr-streaming-conformer-librispeech
+    """
+    def __init__(
+        self,
+        sampling_rate: int = 16_000,
+    ):
+        super().__init__(sampling_rate=sampling_rate)
+        self.config = DynChunkTrainConfig(24, 4)
+        self.norm = True
+        self.model = self.get_model()
+        self.chunk_size = self.model.get_chunk_size_frames(self.config)
+        self.contexts: dict[ID_TYPE, speechbrain.inference.ASR.ASRStreamingContext] = defaultdict(
+            lambda: self.model.make_streaming_context(self.config)
+        )
+    
+    def get_model(self) -> speechbrain.inference.ASR.StreamingASR:
+        model = speechbrain.inference.ASR.StreamingASR.from_hparams( # type: ignore
+            source="speechbrain/asr-streaming-conformer-gigaspeech",
+        )
+        assert model is not None
+        return model
+    
+    @override
+    def _run(self):
+        while True:
+            id, data, is_finished, end_time = self.input_buffer.get_with_rechunking(self.chunk_size)
+            if data is not None:
+                assert isinstance(data, np.ndarray)
+                data_tensor = torch.tensor(data.astype(np.float32), dtype=torch.float32, device='cuda')
+                if self.norm:
+                    data_tensor /= max(1, torch.max(torch.abs(data_tensor)).item())
+                with torch.no_grad():
+                    text = self.model.transcribe_chunk(self.contexts[id], data_tensor.unsqueeze(0))[0]
+                text = text.lstrip()  # speechbrain starts a new word from space
+                if len(text):
+                    self.output_buffer.put(OutputChunk(
+                        data=TranscriptionChunk(text=text),
+                        seconds_processed=end_time,
+                    ), id=id)
+            if is_finished:
+                self.output_buffer.put(OutputChunk(data=Signal.FINISH, seconds_processed=end_time), id=id)
+                self.contexts.pop(id, None)
+    
+    @property
+    @override
+    def audio_type(self) -> Literal['float']:
+        return 'float'
---
+++ tests/test_speechbrain.py
@@ -0,0 +1,28 @@
+from typing import cast
+
+import pytest
+import numpy as np
+import numpy.typing as npt
+import librosa
+
+from asr_eval.streaming.caller import receive_full_transcription
+from asr_eval.streaming.model import TranscriptionChunk, prepare_audio_format
+from asr_eval.streaming.models.speechbrain import SpeechbrainStreaming
+from asr_eval.streaming.sender import StreamingAudioSender
+
+
+@pytest.mark.filterwarnings('ignore::FutureWarning:', 'ignore::DeprecationWarning:')
+def test_speechbrain_streaming():
+    waveform = cast(npt.NDArray[np.floating], librosa.load('tests/testdata/vosk.wav', sr=16_000)[0]) # type: ignore
+    
+    asr = SpeechbrainStreaming()
+    asr.start_thread()
+    
+    array, array_len_per_sec = prepare_audio_format(waveform, asr)
+    sender = StreamingAudioSender(id=0, audio=array, speed_multiplier=5, array_len_per_sec=array_len_per_sec)
+    sender.send_all_without_delays(send_to=asr.input_buffer)
+    
+    chunks = receive_full_transcription(asr=asr, id=sender.id)
+    assert TranscriptionChunk.join(chunks) == 'ONE ZERO ZERO ZERO ONE NINE O TWO ONE O CYRIL ONE AID CYRROW THREE'
+    
+    asr.stop_thread()
