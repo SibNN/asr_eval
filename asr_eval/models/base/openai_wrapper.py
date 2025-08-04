e13d3dcfa9095d2899f4fbd01b14136fc6e73344
Oleg Sedukhin <sedukhin@sibnn.ai>
2025-08-04 12:30:16 +0300

    openai api transcriber

---
+++ asr_eval/models/base/openai_wrapper.py
@@ -0,0 +1,187 @@
+from typing import Any, Literal, cast, override
+import io
+
+from openai import OpenAI, NotGiven, NOT_GIVEN
+from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionTokenLogprob
+from openai.types.audio import Transcription
+from openai.types.audio.transcription_create_params import ChunkingStrategyVadConfig
+from openai.types.audio.transcription import Logprob
+from mistral_common.protocol.instruct.messages import UserContentChunk, TextChunk, AudioChunk, UserMessage
+from mistral_common.audio import Audio
+from pydantic_extra_types.language_code import LanguageAlpha2
+
+from .interfaces import Transcriber
+from ...utils.audio_ops import waveform_to_bytes
+from ...utils.types import FLOATS
+
+
+class APITranscriber(Transcriber):
+    '''
+    A connector to OpenAI API for audio LLMs. Runs via `client.audio.transcriptions.create`.
+    This class wraps api_transcribe() to implement Transcriber interface.
+    
+    Example:
+    
+    1. Start a local VLLM server
+
+    vllm serve mistralai/Voxtral-Mini-3B-2507 --tokenizer_mode mistral --config_format mistral \\
+        --load_format mistral --tensor-parallel-size 1 --tool-call-parser mistral \\
+        --enable-auto-tool-choice --gpu-memory-utilization 0.75
+
+    2. Run the code
+
+    from openai import OpenAI
+    from asr_eval.utils.audio_ops import speech_sample
+    from asr_eval.models.base.openai_wrapper import do_transcribe
+
+    transcriber = APITranscriber(
+        OpenAI(api_key='EMPTY', base_url='http://localhost:8000/v1'),
+        model_name='mistralai/Voxtral-Mini-3B-2507',
+        language='ru',
+    )
+    
+    waveform = speech_sample(repeats=5)
+    transcriber.transcribe(waveform)
+    
+    See api_transcribe() docstring for chunking_strategy and temperature params.
+    '''
+    def __init__(
+        self,
+        client: OpenAI,
+        model_name: str = 'mistralai/Voxtral-Mini-3B-2507',
+        language: str | LanguageAlpha2 = 'ru',
+        chunking_strategy: Literal['auto'] | ChunkingStrategyVadConfig | NotGiven = NOT_GIVEN,
+        temperature: float = 0.7,
+    ):
+        self.client = client
+        self.model_name = model_name
+        self.language = language
+        self.chunking_strategy: Literal['auto'] | ChunkingStrategyVadConfig | NotGiven = chunking_strategy
+        self.temperature = temperature
+    
+    @override
+    def transcribe(self, waveform: FLOATS) -> str:
+        text, _ = api_transcribe(
+            client=self.client,
+            waveform=waveform,
+            language=self.language,
+            model_name=self.model_name,
+            chunking_strategy=self.chunking_strategy,
+            temperature=self.temperature,
+            
+        )
+        return text
+
+  
+def api_transcribe(
+    client: OpenAI,
+    waveform: FLOATS,
+    model_name: str,
+    language: str | LanguageAlpha2 = 'en',
+    chunking_strategy: Literal['auto'] | ChunkingStrategyVadConfig | NotGiven = NOT_GIVEN,
+    temperature: float = 0.7,
+) -> tuple[str, list[Logprob] | None]:
+    '''
+    A connector to OpenAI API for audio LLMs. Runs via `client.audio.transcriptions.create`.
+    See the full usage example in APITranscriber (wrapper around api_transcribe).
+    
+    Sends a message with audio and language to transcribe. A default temperature is 0.7,
+    this value is taken from mistral_common BaseCompletionRequest
+    
+    Returns:
+    - a transcription
+    - logprobs if returned by the model
+    
+    According to openai.types.audio.transcription.Transcription docstring, logprobs
+    are returned only with the models `gpt-4o-transcribe` and `gpt-4o-mini-transcribe`
+    
+    By default chunking_strategy is unset, and the audio is transcribed as a single block,
+    according to `client.audio.transcriptions.create` docstring.
+    
+    Voxtral seem to ignore both chunking_strategy and a request to return logprobs, according
+    to VLLM server logs.
+    
+    Raises:
+    - openai.APIConnectionError if cannot connect to the API
+    - openai.NotFoundError if cannot find the specified model_name
+    - InternalServerError in some cases (happened with VseGPT)
+    '''
+    # flac is actually a zipped (lossess) wav, should have smaller size than wav
+    file = io.BytesIO(waveform_to_bytes(waveform, sampling_rate=16_000, format='flac'))
+    response: Transcription = client.audio.transcriptions.create(
+        file=file,
+        temperature=temperature,
+        model=model_name,
+        language=LanguageAlpha2(language),
+        chunking_strategy=chunking_strategy,
+        include=['logprobs'],
+    )
+    return response.text, response.logprobs
+
+
+def api_chat_completion(
+    client: OpenAI,
+    waveform: FLOATS,
+    model_name: str,
+    logprobs: bool = False,
+    pre_prompt: str | None = None,
+    post_prompt: str | None = None,
+    **generate_kwargs: Any,
+) -> tuple[str, bool, list[ChatCompletionTokenLogprob] | None]:
+    '''
+    A connector to OpenAI API for audio LLMs. Runs via `client.chat.completions.create`.
+    
+    Sends a message with audio, optional pre-prompt and post-prompt.
+    
+    Returns:
+    - a model response
+    - a flag if the response was truncated bue to the length limits
+    - logprobs if logprobs=True
+    
+    Raises:
+    - ContentFilterException if the model refuses to generate
+    - openai.APIConnectionError if cannot connect to the API
+    - openai.NotFoundError if cannot find the specified model_name
+    '''
+    # mistral_common.audio.Audio is a container with data, sampling rate, and format
+    # flac is actually a zipped (lossess) wav, should have smaller size than wav
+    audio = Audio.from_bytes(waveform_to_bytes(waveform, sampling_rate=16_000, format='flac'))
+    
+    messages: list[UserContentChunk] = [AudioChunk.from_audio(audio)]
+    if pre_prompt is not None:
+        messages.insert(0, TextChunk(text=pre_prompt))
+    if post_prompt is not None:
+        messages.append(TextChunk(text=post_prompt))
+    
+    user_message = cast(
+        ChatCompletionUserMessageParam,
+        UserMessage(content=messages).to_openai()
+    )
+    response = client.chat.completions.create(
+        messages=[user_message],
+        model=model_name,
+        stream=False,
+        n=1,
+        logprobs=logprobs,
+        **generate_kwargs,
+    )
+    
+    choice = response.choices[0]
+    assert choice.finish_reason in ('stop', 'length', 'content_filter'), choice.finish_reason
+    if choice.finish_reason == 'content_filter':
+        raise ContentFilterException()
+    
+    returned_logprobs = None
+    if logprobs:
+        assert choice.logprobs is not None and choice.logprobs.content is not None
+        returned_logprobs = choice.logprobs.content
+    
+    return (
+        choice.message.content or '',
+        choice.finish_reason == 'length',
+        returned_logprobs,
+    )
+
+
+class ContentFilterException(RuntimeError):
+    pass
---
+++ asr_eval/utils/audio_ops.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import io
+import tempfile
+from typing import Iterator
+from contextlib import contextmanager
+
+import librosa
+import soundfile as sf
+import numpy as np
+
+from .types import FLOATS
+
+
+def speech_sample(repeats: int = 1) -> FLOATS:
+    waveform = librosa.load('tests/testdata/podlodka_test_0.wav', sr=16_000)[0] # type: ignore
+    return np.concatenate([waveform] * repeats) # type: ignore
+
+
+def waveform_to_bytes(waveform: FLOATS, sampling_rate: int = 16_000, format: str = 'wav') -> bytes:
+    sf.write(buffer := io.BytesIO(), waveform, samplerate=sampling_rate, format=format) # type: ignore
+    buffer.seek(0)
+    return buffer.read()
+
+
+@contextmanager
+def audio_as_file(waveform: FLOATS) -> Iterator[Path]:
+    '''
+    Turns an audio with sampling rate 16_000 into file, TODO check if it works
+    with audio_as_file(waveform) as temp_file_path:
+        recognize_speech(path=temp_file_path)
+    '''
+    with tempfile.NamedTemporaryFile('wb') as f:
+        sf.write(f, waveform, samplerate=16_000, format='wav') # type: ignore
+        yield Path(f.name)
+    f.unlink()
