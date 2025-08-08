1592bed1b58bb6f2fb0e3b6c917d8cb2e061f707
Oleg Sedukhin <sedukhin@sibnn.ai>
2025-08-08 16:59:03 +0300

    delayed imports are not needed anymore

--- asr_eval/bench/pipelines.py
+++ asr_eval/bench/pipelines.py
@@ -5,8 +5,20 @@ import os
 from pathlib import Path
 from typing import Any, override
 
-from ..models.base.longform import ContextualLongformVAD, LongformCTC, LongformVAD
 from ..models.base.interfaces import TimedTranscriber, Transcriber
+from ..models.base.longform import ContextualLongformVAD, LongformCTC, LongformVAD
+from ..models.pyannote_segmenter import PyannoteSegmenter
+from ..models.whisper_wrapper import WhisperLongformWrapper
+from ..models.gigaam_wrapper import GigaAMShortformCTC, GigaAMShortformRNNT
+from ..models.t_one_wrapper import TOneWrapper
+from ..models.vosk54_wrapper import VoskV54
+from ..models.voxtral_wrapper import VoxtralWrapper
+from ..models.yandex_speechkit_wrapper import YandexSpeechKitWrapper
+from ..models.gemma_wrapper import Gemma3nWrapper
+from ..models.legacy_pisets_wrapper import LegacyPisetsWrapper
+from ..models.pisets_wrapper import PisetsWrapper
+from ..models.qwen2_audio_wrapper import Qwen2AudioWrapper
+from ..models.flamingo_wrapper import FlamingoWrapper
 from .datasets import AudioSample
 from ..utils.serializing import save_to_json
 
@@ -120,59 +132,45 @@ class TimedTranscriberPipeline(Pipeline):
 
 class _(TranscriberPipeline, register_as='whisper-tiny'):
     def init(self):
-        from ..models.whisper_wrapper import WhisperLongformWrapper
         return WhisperLongformWrapper('openai/whisper-tiny')
 
 class _(TranscriberPipeline, register_as='whisper-small'):
     def init(self):
-        from ..models.whisper_wrapper import WhisperLongformWrapper
         return WhisperLongformWrapper('openai/whisper-small')
 
 
 class _(TranscriberPipeline, register_as='whisper-large-v3'):
     def init(self):
-        from ..models.whisper_wrapper import WhisperLongformWrapper
         return WhisperLongformWrapper('openai/whisper-large-v3')
 
 
 class _(TranscriberPipeline, register_as='gigaam-ctc'):
     def init(self):
-        from ..models.gigaam_wrapper import GigaAMShortformCTC
         return LongformCTC(GigaAMShortformCTC())
 
 
 class _(TimedTranscriberPipeline, register_as='gigaam-ctc-vad'):
     def init(self):
-        from ..models.gigaam_wrapper import GigaAMShortformCTC
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(GigaAMShortformCTC(), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='gigaam-rnnt-vad'):
     def init(self):
-        from ..models.gigaam_wrapper import GigaAMShortformRNNT
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(GigaAMShortformRNNT(), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='flamingo-ru-vad'):
     def init(self):
-        from ..models.flamingo_wrapper import FlamingoWrapper
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(FlamingoWrapper(lang='ru'), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='gemma3n-ru-vad'):
     def init(self):
-        from ..models.gemma_wrapper import Gemma3nWrapper
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(Gemma3nWrapper(lang='ru'), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='gemma3n-ru-vad-contextual'):
     def init(self):
-        from ..models.gemma_wrapper import Gemma3nWrapper
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return ContextualLongformVAD(
             Gemma3nWrapper(lang='ru'), PyannoteSegmenter(), max_history_words=100
         )
@@ -180,13 +178,11 @@ class _(TimedTranscriberPipeline, register_as='gemma3n-ru-vad-contextual'):
 
 class _(TimedTranscriberPipeline, register_as='pisets-legacy'):
     def init(self):
-        from ..models.legacy_pisets_wrapper import LegacyPisetsWrapper
         return LegacyPisetsWrapper(repo_dir='tmp/pisets_legacy')
 
 
 class _(TimedTranscriberPipeline, register_as='pisets-ru-whisper-large-v3'):
     def init(self):
-        from ..models.pisets_wrapper import PisetsWrapper
         return PisetsWrapper(
             language='ru', recognizer='openai/whisper-large-v3', diarization=None
         )
@@ -194,34 +190,26 @@ class _(TimedTranscriberPipeline, register_as='pisets-ru-whisper-large-v3'):
 
 class _(TimedTranscriberPipeline, register_as='qwen2-audio-vad'):
     def init(self):
-        from ..models.qwen2_audio_wrapper import Qwen2AudioWrapper
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(Qwen2AudioWrapper(), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='t-one-vad'):
     def init(self):
-        from ..models.t_one_wrapper import TOneWrapper
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(TOneWrapper(), PyannoteSegmenter())
 
 
 class _(TimedTranscriberPipeline, register_as='vosk-0.54-vad'):
     def init(self):
-        from ..models.vosk54_wrapper import VoskV54
-        from ..models.pyannote_segmenter import PyannoteSegmenter
         return LongformVAD(VoskV54(), PyannoteSegmenter())
 
 
 class _(TranscriberPipeline, register_as='voxtral-3B'):
     def init(self):
-        from ..models.voxtral_wrapper import VoxtralWrapper
         return VoxtralWrapper('mistralai/Voxtral-Mini-3B-2507', language='ru', local_server_verbose=True)
 
 
 class _(TimedTranscriberPipeline, register_as='yandex-speechkit'):
     def init(self):
-        from ..models.yandex_speechkit_wrapper import YandexSpeechKitWrapper
         return YandexSpeechKitWrapper(
             api_key=os.environ['YANDEX_API_KEY'], language='ru-RU', normalize=False
         )
