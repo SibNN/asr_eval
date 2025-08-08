acbb5bb06ad3d61bc171c95356aac8cbe8cf2be8
Oleg Sedukhin <sedukhin@sibnn.ai>
2025-08-08 18:19:25 +0300

    docstrings

--- asr_eval/streaming/wrappers.py
+++ asr_eval/streaming/wrappers.py
@@ -16,10 +16,13 @@ class Offline(Transcriber):
     '''
     A wrapper that turns StreamingASR into a Transcriber. Transcribes
     the full audio and returns the full transcription.
+    
+    Don't forget to call model.streaming_model.stop_thread() at the end.
     '''
-    def __init__(self, streaming_model: StreamingASR):
+    def __init__(self, streaming_model: StreamingASR, start_thread: bool = True):
         self.streaming_model = streaming_model
-        self.streaming_model.start_thread()
+        if start_thread:
+            self.streaming_model.start_thread()
     
     @override
     def transcribe(self, waveform: FLOATS) -> str:
@@ -36,9 +39,24 @@ class Offline(Transcriber):
 
 
 class QuasiStreaming(StreamingASR):
+    '''
+    Converts non-streaming (offline) ASR model into a streaming one. Calls the offline model
+    with the given `interval` (at the audio timescale).
+    
+    For example, let the audio be 3 seconds long, and interval=1. Will call the offline model:
+    
+    1. On the waveform slice from 0 to 1 second (when enough data received)
+    2. On the waveform slice from 0 to 2 seconds (when enough data received)
+    3. On the waveform slice from 0 to 3 seconds (when enough data received)
+    
+    Each time overwrites the old transcription with the new one (this is achieved by
+    sending a new TranscriptionChunk with the same uid).
+    
+    TODO support batching?
+    TODO set also a real-time minimal interval between model calls
+    TODO add keep=True arg to .get_with_rechunking() instead making of another buffer
+    '''
     def __init__(self, offline_model: Transcriber, interval: float = 0.5):
-        # TODO add keep=True arg to .get_with_rechunking() instead making of another buffer
-        # TODO support batching?
         super().__init__()
         self.offline_model = offline_model
         self.chunk_size = int(16_000 * interval)
