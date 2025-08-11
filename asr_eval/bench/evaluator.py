bbfd37903b8a2f3e1b61a6f688a8c5e1b259f4a0
Oleg Sedukhin <sedukhin@sibnn.ai>
2025-08-11 18:52:12 +0300

    update evaluator

--- asr_eval/bench/evaluator.py
+++ asr_eval/bench/evaluator.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Self, TypedDict, cast
+from typing import Self, TypedDict, cast
 import pickle
 
 import pandas as pd
@@ -10,8 +11,8 @@ from tqdm.auto import tqdm
 from datasets import Dataset
 
 from .datasets import AudioSample, get_dataset
+from ..align.alignment import Alignment
 from ..align.parsing import parse_single_variant_string, parse_multivariant_string
-from ..align.matching import MatchesList, solve_optimal_alignment
 from ..align.transcription import MultiVariantTranscription, SingleVariantTranscription
 from ..utils.serializing import load_from_json
 from ..segments.segment import TimedText
@@ -30,24 +31,58 @@ class Evaluator:
     '''
     def __init__(self, root_dir: str | Path):
         self.root_dir = Path(root_dir)
-        self.df = self._predictions_to_df({})
+        self.df = pd.DataFrame(
+            columns=list(_EvaluatorDataframeRow.__required_keys__)
+        ).set_index('path') # type: ignore
         
         # caches
-        self.ground_truths: dict[str, dict[int, MultiVariantTranscription]] = defaultdict(dict)
+        self.ground_truths: dict[
+            str, dict[int, MultiVariantTranscription | SingleVariantTranscription]
+        ] = defaultdict(dict)
         self._datasets_cache: dict[str, Dataset] = {}
         
-    def load_results(self, skip_loaded: bool = True) -> Self:
-        new_df = self._predictions_to_df({
-            path: self._load_result(path)
-            for path in tqdm(list(self.root_dir.glob('*/*/*/transcription.json')))
-            if not skip_loaded or path not in self.df.index
-        })
-        self.df = pd.concat([self.df, new_df])
+    def load_results(self, skip_loaded: bool = True, max_sample_idx: int | None = None) -> Self:
+        files = [
+            path for path in list(self.root_dir.glob('*/*/*/transcription.json'))
+            # skip if already loaded, if `skip_loaded=True`
+            if (not skip_loaded or path not in self.df.index)
+            # skip if sample index is larger than `max_sample_idx`
+            and (max_sample_idx is None or int(str(path.parent.name)) <= max_sample_idx)
+        ]
+        
+        df_rows: list[_EvaluatorDataframeRow] = []
+        for path in tqdm(files):
+            pipeline_name, dataset_name, sample_idx, _ = path.relative_to(self.root_dir).parts
+            sample_idx = int(sample_idx)
+            true = self.get_ground_truth(dataset_name, sample_idx)
+            result = _TranscriberPipelineResult.from_file(path, true=true)
+            df_rows.append({
+                'path': path,
+                'pipeline_name': pipeline_name,
+                'dataset_name': dataset_name,
+                'sample_idx': sample_idx,
+                'ground_truth': true,
+                'pred': result.pred,
+                'pred_timed': result.pred_timed,
+                'alignment': result.alignment,
+            })
+        
+        if len(df_rows):
+            self.df = pd.concat([
+                self.df,
+                pd.DataFrame(data=df_rows).set_index('path'), # type: ignore
+            ]).groupby(level=0).last() # type: ignore
+        
         return self
+    
+    def _get_dataset(self, dataset_name: str) -> Dataset:
+        if dataset_name not in self._datasets_cache:
+            self._datasets_cache[dataset_name] = get_dataset(dataset_name)()
+        return self._datasets_cache[dataset_name]
     
     def get_ground_truth(
         self, dataset_name: str, sample_idx: int
-    ) -> MultiVariantTranscription:
+    ) -> MultiVariantTranscription | SingleVariantTranscription:
         if sample_idx not in self.ground_truths[dataset_name]:
             dataset = self._get_dataset(dataset_name)
             sample = cast(AudioSample, dataset[sample_idx])
@@ -56,70 +91,60 @@ class Evaluator:
             )
         return self.ground_truths[dataset_name][sample_idx]
     
-    def _predictions_to_df(self, predictions: dict[Path, _SamplePrediction]) -> pd.DataFrame:
-        rows: list[dict[str, Any]] = []
-        for path, pred in predictions.items():
-            rows.append(row := cast(dict[str, Any], pred.copy()))
-            row['path'] = path
-            row['ground_truth'] = (
-                self.get_ground_truth(pred['dataset_name'], pred['sample_idx'])
-            )
-        df = pd.DataFrame(columns=(
-            ['path']
-            + list(_SamplePrediction.__required_keys__)
-            + ['ground_truth']
-        ), data=rows)
-        df = df.set_index('path') # type: ignore
-        return df
 
-    def _load_result(self, path: Path) -> _SamplePrediction:
-        _, dataset_name, sample_idx, _ = path.relative_to(self.root_dir).parts
-        ground_truth = self.get_ground_truth(dataset_name, int(sample_idx))
-        return _sample_prediction_from_file(path, ground_truth)
-    
-    def _get_dataset(self, dataset_name: str) -> Dataset:
-        if dataset_name not in self._datasets_cache:
-            self._datasets_cache[dataset_name] = get_dataset(dataset_name)()
-        return self._datasets_cache[dataset_name]
-
-
-class _SamplePrediction(TypedDict):
-    '''A result of running a Transcriber on a single sample'''
+class _EvaluatorDataframeRow(TypedDict):
+    path: Path
     pipeline_name: str
     dataset_name: str
     sample_idx: int
-    transcription: SingleVariantTranscription
-    alignment: MatchesList
-    timed_transcription: list[TimedText] | None
-    
+    ground_truth: MultiVariantTranscription | SingleVariantTranscription
+    pred: SingleVariantTranscription
+    pred_timed: list[TimedText] | None
+    alignment: Alignment
 
-def _sample_prediction_from_file(
-    path: Path, ground_truth: MultiVariantTranscription
-) -> _SamplePrediction:
-    if (pkl_path := path.with_suffix('.pkl')).exists():
-        return pickle.loads(pkl_path.read_bytes())
-    else:
-        data = load_from_json(path)
-        if data['type'] == 'timed_transcription':
-            timed_transcription = data['output']
-            transcription = ' '.join(seg.text for seg in timed_transcription)
+
+@dataclass
+class _TranscriberPipelineResult:
+    '''
+    A result of running a TranscriberPipeline on a single sample. Keeps the
+    transcription of a single dataset sample and its alignment with ground truth.
+    
+    .from_file() will load a json file with fields "transcription" or "timed_transcription",
+    perform alignment with the given ground truth and return the results.
+    
+    Will cache the results of {file}.json to a neighbour file {file}.pkl, or
+    will load .pkl if it exists.
+    '''
+    pred: SingleVariantTranscription
+    pred_timed: list[TimedText] | None
+    alignment: Alignment
+    
+    @classmethod
+    def from_file(
+        cls,
+        path: Path,
+        true: MultiVariantTranscription | SingleVariantTranscription,
+    ) -> Self:
+        assert path.suffix == '.json'
+        if (pkl_path := path.with_suffix('.pkl')).exists():
+            return pickle.loads(pkl_path.read_bytes())
         else:
-            timed_transcription = None
-            transcription = data['output']
-        
-        transcription = parse_single_variant_string(transcription)
-        alignment, _selected_multivariant_blocks = solve_optimal_alignment(
-            ground_truth.tokens, transcription.tokens
-        )
-        
-        obj: _SamplePrediction = {
-            'pipeline_name': path.parts[-4],
-            'dataset_name': path.parts[-3],
-            'sample_idx': int(path.parts[-2]),
-            'transcription': transcription,
-            'alignment': alignment,
-            'timed_transcription': timed_transcription,
-        }
-        
-        pkl_path.write_bytes(pickle.dumps(obj))
-        return obj
+            data = load_from_json(path)
+            if data['type'] == 'timed_transcription':
+                timed_transcription = data['output']
+                transcription = ' '.join(seg.text for seg in timed_transcription)
+            else:
+                timed_transcription = None
+                transcription = data['output']
+            
+            pred = parse_single_variant_string(transcription)
+            alignment = Alignment.from_predictions(true=true, pred=pred)
+            
+            result = cls(
+                pred=pred,
+                pred_timed=timed_transcription,
+                alignment=alignment,
+            )
+            
+            pkl_path.write_bytes(pickle.dumps(result))
+            return result
